53701dbff6f18d62f15f2801d96db4a949df52a5
Wilson <bianliuzhu@gmail.com>
2018-08-21 13:46:46 +0800

    添加 pipe.js 文件

---
+++ pipe.js
@@ -0,0 +1,17 @@
+/* 
+pipe (管)
+  将两个数据流串起来, 像把两个水管串成一个更长的水管一样
+  
+什么是 pipe 操作 ? [readable(可读的) stream(流)]
+  一个 readable stream 和 一个 writeable stream 串起来后 所有的数据自动从 readable 流 进入 writeable 流 这种操作叫 pipe
+
+在 Node.js 中，Readable 流有一个 pipe()方法
+
+*/
+
+'use strict';
+let fs = require('fs');
+let rs = fs.createReadStream('sample.txt');
+let ws = fs.createWriteStream('copied.txt');
+rs.pipe(ws);
+
