f30aecc0b1ccb27dc89d5a16765e02edf0eafac5
Wilson <bianliuzhu@gmail.com>
2018-08-28 16:23:56 +0800

    修改 pipe.js

--- pipe.js
+++ pipe.js
@@ -13,5 +13,16 @@ pipe (管)
 let fs = require('fs');
 let rs = fs.createReadStream('sample.txt');
 let ws = fs.createWriteStream('copied.txt');
-rs.pipe(ws);
+rs.pipe(ws, {
+  end: false
+});
 
+/* 
+  默认情况下，当Readable流的数据读取完毕，end事件触发后，将自动关闭Writable流。
+  如果我们不希望自动关闭Writable流，需要传入参数：
+
+  readable.pipe(writable, {
+    end: false
+  }); 
+  
+*/
